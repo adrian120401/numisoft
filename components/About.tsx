323ce468bd212463f5bdb770ebbf485d256980f9
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2024-05-25 07:46:12 -0300

    sizes fix

--- components/About.tsx
+++ components/About.tsx
@@ -4,7 +4,7 @@ const About: React.FC = () => {
     return (
         <div className="flex flex-col items-center py-8">
             <h2 className="text-2xl font-bold mb-4 text-center">Sobre nosotros</h2>
-            <div className="flex flex-col md:flex-row items-center px-4 md:px-8 lg:px-16 relative">
+            <div className="flex flex-col md:flex-row items-center px-4 md:px-8 lg:px-16 relative max-w-7xl">
                 <div className='mt-2 md:relative md:mr-[-50px] md:bg-gray-100 rounded-tr-3xl p-4'>
                     <h3 className="text-xl font-bold mb-4 text-center">numisoft</h3>
                     <p className="text-lg">
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,20 +4,20 @@ import Link from 'next/link';
 const Hero: React.FC = () => {
     return (
         <div className="flex items-center justify-center h-[calc(100vh-5rem)]">
-            <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="max-w-7xl mx-auto px-4">
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
                     <div className="flex flex-col justify-center">
-                        <h1 className="text-4xl font-bold mb-4">
+                        <h1 className="text-6xl font-bold mb-4">
                             Lleva tu negocio al próximo nivel
                         </h1>
-                        <p className="text-gray-600 mb-4">
+                        <p className="text-gray-600 text-xl mb-4">
                             Te aydudamos a mejorar el rendimiento y los procesos de tu negocio con
                             software
                         </p>
                         <div className="mt-8">
                             <Link
                                 href={'/contract'}
-                                className="px-4 py-3 bg-slate-700 text-white rounded hover:bg-slate-800"
+                                className="px-4 py-3 bg-slate-700 text-white rounded hover:bg-slate-800 text-2xl"
                             >
                                 Comenzar!
                             </Link>

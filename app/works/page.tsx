006a581825851348bd866dc0dd62f917df62e965
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2024-05-31 06:23:56 -0300

    works page

---
+++ app/works/page.tsx
@@ -0,0 +1,22 @@
+import { projects } from '@/data/works';
+import CardProject from '@/components/CardProject';
+
+export default function Works() {
+    return (
+        <div className="mt-24 px-4">
+            <h1 className="text-3xl font-bold text-center">Trabajos</h1>
+            <div className="flex flex-col items-center mt-4">
+                {projects.map((project, index) => (
+                    <div key={index} className="mb-4">
+                        <CardProject
+                            title={project.title}
+                            description={project.description}
+                            image={project.image}
+                            className=""
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}

dadcfac469fd3cf4f7fc972edd45a8e2881b2646
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2024-06-02 13:42:20 -0300

    fix style

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -8,19 +8,20 @@ const OPTIONS: EmblaOptionsType = { loop: true };
 
 const Project: React.FC = () => {
     const SLIDES = projects.map((project, index) => (
-        <CardProject
-            key={project.id}
-            id={project.id}
-            title={project.title}
-            description={project.description}
-            image={project.image}
-        />
+        <div key={project.id} className='p-1'>
+            <CardProject
+                id={project.id}
+                title={project.title}
+                description={project.description}
+                image={project.image}
+            />
+        </div>
     ));
 
     return (
         <section className="py-8">
             <h2 className="text-2xl font-bold mb-8 text-center">Algunos trabajos</h2>
-            <div className="">
+            <div className="w-full h-full">
                 <EmblaCarousel slides={SLIDES} options={OPTIONS} />
             </div>
         </section>

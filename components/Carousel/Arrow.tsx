71dc809361df9e70caac2e0f048f4ab65906fa1a
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2024-06-02 14:11:47 -0300

    add use client in carousel

--- components/Carousel/Arrow.tsx
+++ components/Carousel/Arrow.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { PropsWithChildren, useCallback, useEffect, useState } from 'react';
 import { EmblaCarouselType } from 'embla-carousel';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
